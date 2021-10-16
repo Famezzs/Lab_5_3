932dd3db62aff12dc44a3dc6963529a9356c46c7
Famezzs <Andrii.Kozubenko.IT.2021@lpnu.ua>
2021-10-16 17:48:38 +0300

    Added the project

---
+++ Lab_5_3/Lab_5_3.cpp
@@ -0,0 +1,62 @@
+﻿// Lab_5_3.cpp
+// Козубенко Андрій
+// Лабораторна робота № 5.3
+// Функції, що містять розгалуження та цикли з рекурентними співвідношеннями.
+// Варіант 10
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+double y(const double x);
+
+int main()
+{
+	double fp, fk;
+	double z; // результат обчислень виразу
+	int n;
+
+	cout << "fp = "; cin >> fp;
+	cout << "fk = "; cin >> fk;
+	cout << "n = "; cin >> n;
+
+	cout << fixed;
+	cout << "---------------------------------" << endl;
+	cout << "|" << setw(15) << "       f       " << "|" << setw(15) << "       z       " << "|" << endl;
+	cout << "---------------------------------" << endl;
+
+	double df = (fk - fp) / n * 1.;
+
+	for (double f = fp; f <= fk; f += df)
+	{
+		z = y(f / 2.) + y(f + 1) * y(f + 1) + y(2. * f);
+
+		cout << fixed;
+		cout << "|" << setw(15) << right << setprecision(4) << f << "|" << setw(15) << right << setprecision(4) << z << "|" << endl;
+	}
+	cout << "---------------------------------";
+
+	return 0;
+}
+
+double y(const double x)
+{
+	if (abs(x) >= 1)
+		return exp(x) / (1 + exp(x) + sin(1. * x)) * 1.;
+	else
+	{
+		double S = 0;
+		int j = 0;
+		double a = 1;
+		S = a;
+		do {
+			j++;
+			double R = (-1.) * x * x / j;
+			a *= R;
+			S += a;
+		} while (j < 8);
+
+		return S;
+	}
+}
---
+++ UnitTest_5_3(I)/UnitTest_5_3(I).cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\Home\source\repos\Lab_5_3\Lab_5_3\Lab_5_3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest53I
+{
+	TEST_CLASS(UnitTest53I)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t = y(0.);
+			Assert::AreEqual(t, 1.);
+
+			t = y(5.);
+			Assert::IsTrue(t > 0.999723); // друге число є приблизним результатом, що обчислений за допомогою калькулятора
+		}
+	};
+}
